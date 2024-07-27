9a884e73b44eaeacab563d78963c61c871a09f6f
KiReally76L <141634933+KiReally76L@users.noreply.github.com>
2024-07-27 18:15:46 +0400

    add main components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,11 @@
+import { Movies } from '@/components/Movies'
+
 export const Home = () => {
-	return <div></div>
+	return (
+		<div className='w-full flex justify-center'>
+			<div className='w-[1280px]'>
+				<Movies />
+			</div>
+		</div>
+	)
 }
